e7f78f2591d8fb2928c266aef4356342221e5eff
Cooper <79376063+Cooper14052@users.noreply.github.com>
2023-01-29 13:53:23 +0300

    version 2

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import win10toast###
+from win10toast import ToastNotifier###
 import requests
 import folium
-
+from tkinter import *###
+import webbrowser as wb
 
 def get_info_ip(ip):
     try:
         req = requests.get(url=f'http://ip-api.com/json/{ip}').json()
-
         data = {
             '[IP]': req.get('query'),
             '[Int prov]': req.get('isp'),
@@ -16,19 +18,43 @@ def get_info_ip(ip):
             '[Lon]': req.get('lon'),
         }
 
-        for k,v in data.items():
+        for k, v in data.items():
             print(f'{k}: {v}')
-        area = folium.Map(location=[req.get('lat'),req.get('lon')])
+        with open('info.txt', 'a') as f:
+            for k, v in data.items():
+                f.write(f'{k}: {v}\n')
+            f.write(f'--------------------\n')
+        area = folium.Map(location=[req.get('lat'), req.get('lon')])
         area.save(f'{req.get("query")}_{req.get("city")}.html')
+
+        ip = req.get("query")
+        city = req.get("city")
+        wb.open(f'file:///C:/Users/Cooper/PycharmProjects/OSINT/{ip}_{city}.html')
     except:
         print()
-
 
 def main():
     ip = (input('Введите ip-адрес:'))
 
     get_info_ip(ip=ip)
 
+def message():
+    message = win10toast.ToastNotifier()
+    message.show_toast(title="OSINT", msg="Scan finished!", duration=10)
+
+def window():
+    window = Tk()
+    window.title('Инфо')
+    window.geometry('300x250')
+    window.wm_attributes('-alpha', 0.7)
+    title = Label(window,text='Введите ip')
+    title.pack()
+    window.mainloop()
 
 if __name__ == '__main__':
-    main()
+    try:
+        while True:
+            main()
+            #message()
+    except:
+        print()
