6e7d05c3721b14f4472e22519205982a0744fc19
Cooper <79376063+Cooper14052@users.noreply.github.com>
2023-01-31 14:00:37 +0300

    version 3.1.1

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import requests
 import folium
 from tkinter import *
 import webbrowser as wb
+import pyfiglet
 
+name_img = pyfiglet.figlet_format("Firebrand")
+print(name_img)
 
 def get():
     ip = edit.get()
@@ -10,16 +13,17 @@ def get():
         req = requests.get(url=f'http://ip-api.com/json/{ip}').json()
         data = {
             '[IP]': req.get('query'),
-            '[Int prov]': req.get('isp'),
-            '[Country]': req.get('country'),
-            '[Region]': req.get('regionName'),
-            '[ZIP]': req.get('zip'),
-            '[Lat]': req.get('lat'),
-            '[Lon]': req.get('lon'),
+            '[Интернет провайдер]': req.get('isp'),
+            '[Страна]': req.get('country'),
+            '[Регион]': req.get('regionName'),
+            '[Почтовый индекс]': req.get('zip'),
+            '[Широта]': req.get('lat'),
+            '[Долгота]': req.get('lon'),
         }
 
         for k, v in data.items():
             print(f'{k}: {v}')
+        print(f'--------------------\n')
         with open('info.txt', 'a') as f:
             for k, v in data.items():
                 f.write(f'{k}: {v}\n')
@@ -33,16 +37,17 @@ def get():
     except:
         print()
 
+
 window = Tk()
-#window.iconbitmap('favpng_vector-graphics-royalty-free-stock-illustration-knight.ico')
-window.title('Knight')
+window.iconbitmap('d80f3480a0a42895570ec5c660263700 (1).ico')
+window.title('Firebrand')
 window.geometry('300x250')
 window.wm_attributes('-alpha', 0.8)
 
-title = Label(window, text='Введите ip')
+title = Label(window, text='Введите IPv4')
 title.pack()
 
-edit = Entry(window)
+edit = Entry(window,width=15)
 edit.pack()
 
 but = Button(text='Принять', command=get)
@@ -50,3 +55,5 @@ but.pack()
 
 window.mainloop()
 
+if __name__ == '__main__':
+    pass
