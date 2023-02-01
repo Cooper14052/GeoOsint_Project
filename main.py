1b46f289f8dcce2cc9bf1e6eca9f57eea0960e4d
Cooper <79376063+Cooper14052@users.noreply.github.com>
2023-02-01 22:42:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import folium
 from tkinter import *
 import webbrowser as wb
 import pyfiglet
+import os
+import time
 
 name_img = pyfiglet.figlet_format("Firebrand")
 print(name_img)
+
+def open_info():
+    os.startfile('info.txt')
 
 def get():
     ip = edit.get()
@@ -20,26 +25,26 @@ def get():
             '[Широта]': req.get('lat'),
             '[Долгота]': req.get('lon'),
         }
-
         for k, v in data.items():
             print(f'{k}: {v}')
         print(f'--------------------\n')
-        with open('info.txt', 'a') as f:
+        with open('info.txt', 'a', encoding='UTF-8') as f:
             for k, v in data.items():
                 f.write(f'{k}: {v}\n')
             f.write(f'--------------------\n')
         area = folium.Map(location=[req.get('lat'), req.get('lon')])
         area.save(f'{req.get("query")}_{req.get("city")}.html')
-
+        
         ip = req.get("query")
         city = req.get("city")
         wb.open(f'file:///C:/Users/Cooper/PycharmProjects/OSINT/{ip}_{city}.html')
+        time.sleep(1)
+        os.remove(f'{req.get("query")}_{req.get("city")}.html')
     except:
         print()
 
-
 window = Tk()
-window.iconbitmap('d80f3480a0a42895570ec5c660263700 (1).ico')
+window.iconbitmap('icon.ico')
 window.title('Firebrand')
 window.geometry('300x250')
 window.wm_attributes('-alpha', 0.8)
@@ -53,7 +58,7 @@ edit.pack()
 but = Button(text='Принять', command=get)
 but.pack()
 
-window.mainloop()
+but1 = Button(text='История', command=open_info)
+but1.place(x=240, y=220)
 
-if __name__ == '__main__':
-    pass
+window.mainloop()
